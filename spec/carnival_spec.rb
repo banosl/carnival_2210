ce7beca93647848f0142984ee5a9d20850b9581e
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 10:21:54 -0700

    Feat: initialize carnival with attr_reader

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -5,9 +5,10 @@ require 'pry'
 
 RSpec.describe Carnival do
   it 'carnival exists and has attributes' do
-    carnival = Carnival.new("5 days")
+    carnival = Carnival.new("Frozen Dead Guy Days", "5 days")
 
     expect(carnival).to be_instance_of(Carnival)
+    expect(carnival.name).to eq("Frozen Dead Guy Days")
     expect(carnival.duration).to eq("5 days")
     expect(carnival.rides).to eq([])
   end
