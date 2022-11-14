43662d3e57d54470c43449eb15eb0037d8f12437
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 09:05:50 -0700

    Test: visitor exists and has attributes

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,15 @@
+require './lib/visitor'
+require 'pry'
+
+RSpec.describe Visitor do
+  it 'visitor exists and has attributes' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1).to be_instance_of(Visitor)
+    expect(visitor1.name).to eq('Bruce')
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).eq (10)
+  end
+
+
+end
