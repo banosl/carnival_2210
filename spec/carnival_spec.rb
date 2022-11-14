72e0677f34c5e1e9586599751c569ac04eb9f33b
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 10:29:53 -0700

    Test: added more to the before each

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -6,6 +6,29 @@ require 'pry'
 RSpec.describe Carnival do
   before(:each) do
     @carnival = Carnival.new("Frozen Dead Guy Days", "5 days")
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor3.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor1)
+    @ride2.board_rider(@visitor1)
+    @ride2.board_rider(@visitor2)
+    @ride2.board_rider(@visitor3)
+    @ride3.board_rider(@visitor1)
+    @ride3.board_rider(@visitor2)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
   end
   
   it 'carnival exists and has attributes' do
