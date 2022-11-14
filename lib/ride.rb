b2354d32ed1fd1197f0cd4f461e55c3690c09c96
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 09:29:53 -0700

    Feat add attribute ride_log

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,12 +3,14 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :ride_log
   def initialize(attributes)
     @name = attributes[:name]
     @min_height = attributes[:min_height]
     @admission_fee = attributes[:admission_fee]
     @excitement = attributes[:excitement]
     @total_revenue = 0
+    @ride_log = {}
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,5 +12,13 @@ RSpec.describe Ride do
     expect(ride1.admission_fee).to eq(1)
     expect(ride1.excitement).to eq(:gentle)
     expect(ride1.total_revenue).to eq(0)
+    expect(ride1.ride_log).to eq({})
+  end
+
+  it 'ride can board_riders' do
+    ride1 = Ride.new({name: "Carousel", min_height: 24, admission_fee: 1, excitement: :gentle})
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+
   end
 end
