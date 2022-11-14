b592957cdb7994fc78793c3f5b7ebf86fd21530c
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 10:24:49 -0700

    Test: added before each

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,12 +4,16 @@ require './lib/visitor'
 require 'pry'
 
 RSpec.describe Carnival do
-  it 'carnival exists and has attributes' do
-    carnival = Carnival.new("Frozen Dead Guy Days", "5 days")
-
-    expect(carnival).to be_instance_of(Carnival)
-    expect(carnival.name).to eq("Frozen Dead Guy Days")
-    expect(carnival.duration).to eq("5 days")
-    expect(carnival.rides).to eq([])
+  before(:each) do
+    @carnival = Carnival.new("Frozen Dead Guy Days", "5 days")
   end
+  
+  it 'carnival exists and has attributes' do
+    expect(@carnival).to be_instance_of(Carnival)
+    expect(@carnival.name).to eq("Frozen Dead Guy Days")
+    expect(@carnival.duration).to eq("5 days")
+    expect(@carnival.rides).to eq([])
+  end
+
+  
 end
